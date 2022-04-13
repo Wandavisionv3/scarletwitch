688947813f01cdb6a3ee3210601b2fd69539c283
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-13 12:43:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -84,7 +84,7 @@ Note:- Give a user details
 - Filter is the feature were users can set automated replies for a particular keyword and ᗩᒍᗩ᙭  will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. ᗩᒍᗩ᙭ should have admin privillage.
+1. 𝖧𝗂𝖼𝗁𝗈𝗓 should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -189,11 +189,11 @@ Delete A Lot Of Messages From Groups!
 ◉ /purge :- Delete All Messages From The Replied To Message, To The Current Message"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
--ᗩᒍᗩ᙭  Supports both url and alert inline buttons.
+-𝖧𝗂𝖼𝗁𝗈𝗓  Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. ᗩᒍᗩ᙭ supports buttons with any telegram media type.
+2. 𝖧𝗂𝖼𝗁𝗈𝗓 supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -228,7 +228,7 @@ Delete A Lot Of Messages From Groups!
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of ᗩᒍᗩ᙭ 
+these are the extra features of 𝖧𝗂𝖼𝗁𝗈𝗓 
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
