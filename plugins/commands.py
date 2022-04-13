b9956d8a7c0d836f0f09d5eecc5bb12c4ac53f62
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-13 10:15:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -62,7 +62,7 @@ async def start(client, message):
         await message.reply_chat_action("Typing")
         m=await message.reply_sticker("𝖢𝗋𝖾𝖽𝗂𝗍:[𝖺𝗃𝖺𝗑](https://t.me/Devil0Bot_𝖡𝗈𝗍")
 
-𝖫𝗂𝗄𝖾 𝗍𝗁𝗂𝗌) 
+
         await asyncio.sleep(2)
         await m.delete()
         return
