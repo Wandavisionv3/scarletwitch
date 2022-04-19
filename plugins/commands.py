ae7e58dc209751fb2f434a36b0c27cc8de647a28
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-19 08:54:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -61,7 +61,7 @@ async def start(client, message):
             parse_mode='html'
         )
         await message.reply_chat_action("typing")
-        m=await message.reply_sticker(sticker="CAACAgUAAxkBAAI1_2Hnz0usiwmy7XolQ0i-HoCp7AABhgACFQEAAsiUZBRmRDCipxVsEx4E", reply_markup=reply_markup, reply_to_message_id=message.message_id) 
+        m=await message.reply_sticker(sticker="CAACAgUAAxkBAAI1_2Hnz0usiwmy7XolQ0i-HoCp7AABhgACFQEAAsiUZBRmRDCipxVsEx4E") 
 
         await asyncio.sleep(2)
         await m.delete()
