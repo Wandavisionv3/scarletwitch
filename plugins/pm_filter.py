836a8145a3941d22db5e727cda03e28319d6a926
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-20 20:31:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -704,15 +704,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('⚚ ᗩDD ME TO YOUR GROUP ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHANNEL 💠', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
-            InlineKeyboardButton('💠 GROUP 💠', url='https://t.me/+JLuNC2rGfgQ0OGRl')
+            InlineKeyboardButton(' CHANNEL 📡', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
+            InlineKeyboardButton(' GROUP ✨', url='https://t.me/+JLuNC2rGfgQ0OGRl')
             ],[      
-            InlineKeyboardButton('♻️ HELP ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ᗩBOUT ♻️', callback_data='about')
+            InlineKeyboardButton(' HELP 🌀', callback_data='help'),
+            InlineKeyboardButton(' ᗩBOUT 🛰️', callback_data='about')
             ],[
-            InlineKeyboardButton('💠 SEᗩRCH ᕼERE 💠', switch_inline_query_current_chat='')
+            InlineKeyboardButton(' SEᗩRCH ᕼERE 🔍', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton('🔐 CLOSE AND EXIT 🔐', callback_data='close_data')
+            InlineKeyboardButton(' CLOSE AND EXIT 🤐', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
