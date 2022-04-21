d4a2ab1c4702606f02d140efa5155849d15290a6
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-21 09:53:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,13 +44,13 @@ async def start(client, message):
         buttons = [[
                      InlineKeyboardButton('⚚ ᗩDD ME TO YOUR GROUP ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🎬 CHANNEL 🎬', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
-            InlineKeyboardButton('✨ GROUP ✨', url='https://t.me/+JLuNC2rGfgQ0OGRl')
+            InlineKeyboardButton('💠 CHANNEL 💠', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
+            InlineKeyboardButton('💠 GROUP 💠', url='https://t.me/+JLuNC2rGfgQ0OGRl')
             ],[      
-            InlineKeyboardButton('⚙ HELP ⚙', callback_data='help'),
-            InlineKeyboardButton('🦾 ᗩBOUT 🦾', callback_data='about')
+            InlineKeyboardButton('♻️ HELP ♻️', callback_data='help'),
+            InlineKeyboardButton('💠 ᗩBOUT 💠', callback_data='about')
             ],[
-            InlineKeyboardButton('🔍 SEᗩRCH ᕼERE 🔍', switch_inline_query_current_chat='')
+            InlineKeyboardButton('💠 SEᗩRCH ᕼERE 💠', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('🖇 CLOSE AND EXIT 🖇', callback_data='close_data')
         ]]
