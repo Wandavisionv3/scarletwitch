525835e6ded00429b1fe8b6f823d4aadba9bfc1b
Jonndeb <94781329+Jonndeb@users.noreply.github.com>
2022-04-21 11:15:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,15 +44,15 @@ async def start(client, message):
         buttons = [[
                      InlineKeyboardButton('⚚ ᗩDD ME TO YOUR GROUP ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton(' CHANNEL ', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
-            InlineKeyboardButton(' GROUP ', url='https://t.me/+JLuNC2rGfgQ0OGRl')
+            InlineKeyboardButton('🎬 CHANNEL 🎬', url='https://t.me/+hpnKBqJC_cQ3ZjU1'),
+            InlineKeyboardButton('🎥 GROUP 🎥', url='https://t.me/+JLuNC2rGfgQ0OGRl')
             ],[      
-            InlineKeyboardButton(' HELP ', callback_data='help'),
-            InlineKeyboardButton(' ᗩBOUT ', callback_data='about')
+            InlineKeyboardButton('⚙ HELP ⚙', callback_data='help'),
+            InlineKeyboardButton('🦾 ᗩBOUT 🦾', callback_data='about')
             ],[
-            InlineKeyboardButton(' SEᗩRCH ᕼERE ', switch_inline_query_current_chat='')
+            InlineKeyboardButton('🔍 SEᗩRCH ᕼERE 🔍', switch_inline_query_current_chat='')
             ],[
-            InlineKeyboardButton(' CLOSE AND EXIT ', callback_data='close_data')
+            InlineKeyboardButton('🚪 CLOSE AND EXIT 🚪', callback_data='close_data')
         ]]
         await message.reply_chat_action("typing")
         m=await message.reply_sticker(sticker="CAACAgUAAxkBAAI1_2Hnz0usiwmy7XolQ0i-HoCp7AABhgACFQEAAsiUZBRmRDCipxVsEx4E") 
