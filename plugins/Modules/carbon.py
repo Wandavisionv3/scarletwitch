82becb51a28081a4330c756a1dfa3b1bec432bcc
Mhhydra25 <96157167+Mhhydra25@users.noreply.github.com>
2022-04-24 18:22:57 +0530

    Update carbon.py

--- plugins/Modules/carbon.py
+++ plugins/Modules/carbon.py
@@ -5,10 +5,10 @@ from plugins.function import make_carbon
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 aiohttpsession = ClientSession()
 
-C = "**𝙼𝙰𝙳𝙴 𝙱𝚈 [𝖧𝗂𝖼𝗁𝗈𝗓](https://t.me/Hichozbot)**"
+C = "**𝙼𝙰𝙳𝙴 𝙱𝚈 [Ꮐᖇᗴᗴᑎ ᏞᗩᑎᎢᕼᗴᖇᑎ 🐉](https://t.me/Greenlanthernv6_bot)**"
 F = InlineKeyboardMarkup(
 [[
-     InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚉 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/+hpnKBqJC_cQ3ZjU1")
+     InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚉 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url="https://t.me/Newcinemazz")
 ]]
 )
 
