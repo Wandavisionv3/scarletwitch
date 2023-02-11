b31fec7d6903ff9d68cf6a0da4fdbf732526bf02
Scarletwitch <101384551+Wandavisionv3@users.noreply.github.com>
2023-02-11 21:11:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ BOT_TOKEN = environ['5084165351:AAFa5n1etvM5Y9r0QLuDEd1EYayY6vm4BXE']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/4d485fb00ad3dfc8f970d.jpg  ')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/4d485fb00ad3dfc8f970d.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
